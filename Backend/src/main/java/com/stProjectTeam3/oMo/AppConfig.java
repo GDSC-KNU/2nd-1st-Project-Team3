d9897d518aff15aecba0495391f9eb891dbf1cfb
TenJeong <33208278+TenJeong@users.noreply.github.com>
2023-03-20 19:33:23 +0900

    feat: CORS 임시 적용

--- Backend/src/main/java/com/stProjectTeam3/oMo/AppConfig.java
+++ Backend/src/main/java/com/stProjectTeam3/oMo/AppConfig.java
@@ -3,9 +3,17 @@ package com.stProjectTeam3.oMo;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan
 @PropertySource("classpath:application.properties")
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
 }
