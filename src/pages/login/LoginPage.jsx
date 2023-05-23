3f2d237af752e190e754eee9d5e1a6fac9760b15
hwiiy <87405971+hwiiy@users.noreply.github.com>
2023-05-23 20:17:53 +0900

    feat : add LoginPage, SignupPage, store.js

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -14,14 +16,38 @@ const LoginPage = () => {
     setPassword(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const goToMain = () => {
+    navigate("/main");
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // todo : 로그인 처리 로직 작성
-
     console.log("로그인 정보:", username, password);
+
+    try {
+      const response = await axios.post("https://ottmowa.kro.kr/login", {
+        username,
+        password,
+      });
+
+      const { data } = response;
+
+      if (data) {
+        setUser(data);
+      } else {
+        alert("아이디 혹은 비밀번호가 일치하지 않습니다.");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("로그인 실패");
+    }
   };
-  
+
+  useEffect(() => {
+    if (user) {
+      goToMain();
+    }
+  }, [user]);
   const handleSignupClick = () => {
     navigate("/signup");
   };
--- src/pages/signup/SignupPage.jsx
+++ src/pages/signup/SignupPage.jsx
@@ -1,47 +1,194 @@
+// import React, { useState } from "react";
+
+// const SignupPage = () => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleUsernameChange = (e) => {
+//     setUsername(e.target.value);
+//   };
+
+//   const handlePasswordChange = (e) => {
+//     setPassword(e.target.value);
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     // todo : 회원가입 처리 로직 작성
+
+//     console.log("회원가입 정보:", username, password);
+
+//     // todo : 회원가입 완료 후 리다이렉트 등 처리할 수 있음
+//   };
+
+//   return (
+//     <div>
+//       <h1>회원가입</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label>사용자명:</label>
+//           <input type="text" value={username} onChange={handleUsernameChange} />
+//         </div>
+//         <div>
+//           <label>비밀번호:</label>
+//           <input
+//             type="password"
+//             value={password}
+//             onChange={handlePasswordChange}
+//           />
+//         </div>
+//         <button type="submit">가입하기</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default SignupPage;
+
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import styled from "styled-components";
 
-const SignupPage = () => {
-  const [username, setUsername] = useState("");
+const SignupWrapper = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FormGroup = styled.div`
+  margin-bottom: 15px;
+`;
+
+const Label = styled.label`
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  padding: 8px;
+  font-size: 14px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 10px;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const Signup = () => {
+  const navigate = useNavigate();
+  const [account, setAccount] = useState("");
   const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
+  const handleAccountChange = (e) => {
+    setAccount(e.target.value);
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleNicknameChange = (e) => {
+    setNickname(e.target.value);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // todo : 회원가입 처리 로직 작성
+    const userData = {
+      account,
+      password,
+      nickname,
+      name,
+      email,
+    };
 
-    console.log("회원가입 정보:", username, password);
+    console.log(userData);
 
-    // todo : 회원가입 완료 후 리다이렉트 등 처리할 수 있음
+    try {
+      const response = await axios.post(
+        "https://ottmowa.kro.kr/register",
+        userData
+      );
+      console.log(response.data);
+
+      alert("회원가입이 완료되었습니다.");
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+      alert("회원가입 실패");
+    }
+  };
+
+  const handleLoginClick = () => {
+    navigate("/login");
   };
 
   return (
-    <div>
-      <h1>회원가입</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>사용자명:</label>
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </div>
-        <div>
-          <label>비밀번호:</label>
-          <input
+    <SignupWrapper>
+      <Title>회원가입</Title>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label>아이디:</Label>
+          <Input type="text" value={account} onChange={handleAccountChange} />
+        </FormGroup>
+        <FormGroup>
+          <Label>비밀번호:</Label>
+          <Input
             type="password"
             value={password}
             onChange={handlePasswordChange}
           />
-        </div>
-        <button type="submit">가입하기</button>
-      </form>
-    </div>
+        </FormGroup>
+        <FormGroup>
+          <Label>닉네임:</Label>
+          <Input type="text" value={nickname} onChange={handleNicknameChange} />
+        </FormGroup>
+        <FormGroup>
+          <Label>이름:</Label>
+          <Input type="text" value={name} onChange={handleNameChange} />
+        </FormGroup>
+        <FormGroup>
+          <Label>이메일:</Label>
+          <Input type="email" value={email} onChange={handleEmailChange} />
+        </FormGroup>
+        <Button type="submit">회원가입</Button>
+        <Button type="button" onClick={handleLoginClick}>
+          로그인
+        </Button>
+      </Form>
+    </SignupWrapper>
   );
 };
 
-export default SignupPage;
+export default Signup;
